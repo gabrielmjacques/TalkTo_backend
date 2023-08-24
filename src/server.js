be17af75c3ba5d20d8afe474e7bdf727fa25cfe7
gabrielmjacques <egmoreira10@gmail.com>
2023-08-24 11:40:56 -0300

    feat: add WebSocket connection support

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
-const express = require( 'express' );
-const routes = require( './routes' );
+const app = require( 'express' )();
+const server = require( 'http' ).createServer( app );
+const io = require( 'socket.io' )( server, { cors: { origin: 'http://localhost:5173' } } );
 
-const app = express();
 
-app.use( express.json() );
-app.use( routes );
+io.on( 'connection', socket => {
+    console.log( `User: ${ socket.id } as connected` )
 
-app.listen( 3000, () => {
+    socket.on( 'disconnect', reason => {
+        console.log( `User ${ socket.data.username } as diconnected` )
+    } )
+
+    socket.on( 'set_username', username => {
+        socket.data.username = username
+
+        console.log( `Name setted as ${ username }` )
+    } )
+} )
+
+
+server.listen( 3000, () => {
     console.log( 'Server running on port 3000' );
 } )
